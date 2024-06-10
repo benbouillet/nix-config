f272788a45b2cf334051f155c0c328337376d350
Ben Bouillet <15980664+benbouillet@users.noreply.github.com>
2024-06-10 23:11:45 +0200

    feat(nvim): added nixvim

--- flake.nix
+++ flake.nix
@@ -2,23 +2,38 @@
   description = "Example kickstart Nix on macOS environment.";
 
   inputs = {
-    darwin.inputs.nixpkgs.follows = "nixpkgs";
-    darwin.url = "github:lnl7/nix-darwin";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    darwin = {
+      url = "github:lnl7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+
     homebrew-core = {
       url = "github:homebrew/homebrew-core";
       flake = false;
     };
+
     homebrew-cask = {
       url = "github:homebrew/homebrew-cask";
       flake = false;
     };
+
     nixpkgs = {
       url = "github:NixOS/nixpkgs/nixos-24.05";
     };
+
     mac-app-util.url = "github:hraban/mac-app-util";
+
+    nixvim = {
+      url = "github:nix-community/nixvim/nixos-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs @ {
