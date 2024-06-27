73a005e14d3447780d056c1eca2ae5af4dae837a
ben <15980664+benbouillet@users.noreply.github.com>
2024-06-27 11:00:53 +0200

    fix(brew): homebrew now managed via nix

--- flake.nix
+++ flake.nix
@@ -23,12 +23,32 @@
       url = "github:lnl7/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs-darwin";
     };
+
+    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+
+    # Optional: Declarative tap management
+    homebrew-core = {
+      url = "github:homebrew/homebrew-core";
+      flake = false;
+    };
+    homebrew-cask = {
+      url = "github:homebrew/homebrew-cask";
+      flake = false;
+    };
+    homebrew-bundle = {
+      url = "github:homebrew/homebrew-bundle";
+      flake = false;
+    };
+    homebrew-services = {
+      url = "github:homebrew/homebrew-services";
+      flake = false;
+    };
   };
 
 ###################################################
 ################### OUTPUTS #######################
 ###################################################
-    outputs = inputs @ { self, nixpkgs, darwin, home-manager, ... }: let
+    outputs = inputs @ { self, nixpkgs, darwin, home-manager, nix-homebrew, homebrew-core, homebrew-cask, homebrew-bundle, homebrew-services,... }: let
     username = "ben";
     useremail = "15980664+benbouillet@users.noreply.github.com";
     system = "aarch64-darwin"; # aarch64-darwin or x86_64-darwin
@@ -68,6 +88,32 @@
             home-manager.sharedModules = [
               inputs.nixvim.homeManagerModules.nixvim
             ];
+          }
+
+          nix-homebrew.darwinModules.nix-homebrew {
+            nix-homebrew = {
+              # Install Homebrew under the default prefix
+              enable = true;
+  
+              # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
+              enableRosetta = true;
+  
+              # User owning the Homebrew prefix
+              user = username;
+  
+              # Optional: Declarative tap management
+              taps = {
+                "homebrew/homebrew-core" = homebrew-core;
+                "homebrew/homebrew-cask" = homebrew-cask;
+                "homebrew/homebrew-bundle" = homebrew-bundle;
+                "homebrew/homebrew-services" = homebrew-services;
+              };
+  
+              # Optional: Enable fully-declarative tap management
+              #
+              # With mutableTaps disabled, taps can no longer be added imperatively with `brew tap`.
+              mutableTaps = false;
+            };
           }
         ];
       };
