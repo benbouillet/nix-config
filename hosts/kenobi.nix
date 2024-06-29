1a9a8ce8edfeda08fc493c32dec4578497aa9a9c
ben <15980664+benbouillet@users.noreply.github.com>
2024-06-29 16:55:09 +0200

    feat(apps): updated kenobi config

--- hosts/kenobi.nix
+++ hosts/kenobi.nix
@@ -1,20 +1,26 @@
+{ pkgs, ... }:
+
 {
+  pkgs = [
+    pkgs.podman
+    pkgs.podman-tui
+    pkgs.skhd
+    pkgs.yabai
+    pkgs.tailscale
+    # "brave" # not available on aarch64-apple-darwin
+    # "steam" # not available on aarch64-apple-darwin
+  ];
+
   casks = [
     "whatsapp"
     "brave-browser"
     "steam"
+    "obsidian"
+    "discord"
+    "spotify"
+    "whatsapp"
     # not available (nixpkgs nor brew)
     # "arkenforge"
     # "DungeonDraft"
-  ];
-  pkgs = [
-    # "brave" # not available on aarch64-apple-darwin
-    "tailscale"
-    "docker"
-    "obsidian"
-    "slack"
-    "discord"
-    "spotify"
-    # "steam" # not available on aarch64-apple-darwin
   ];
 }
