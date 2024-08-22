e19639b90b21f58aba12792660808849a7f8d895
Ben Bouillet <15980664+benbouillet@users.noreply.github.com>
2024-08-22 19:03:23 +0200

    feat(hardware): T480 configuration

--- flake.nix
+++ flake.nix
@@ -1,15 +1,23 @@
 {
-  description = "A very basic flake";
+  description = "My Thinkpad T480 configuration";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixos-hardware.url = "github:nixos/nixos-hardware?ref=master";
   };
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+  outputs = inputs@{ self, nixpkgs, ... }: 
+  let
+    system = "aarch64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in
+  {
+    nixosConfigurations.nixos = {
+      modules = [
+        ./nixos/configuration.nix
+	./nixos/hardware-configuration.nix
+	inputs.nixos-hardware.nixosModules.lenovo-thinkpad-t480
+      ];
+    };
   };
 }
