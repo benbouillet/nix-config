430358d43af2a9f024131be86f2270c452897b6a
Ben Bouillet <15980664+benbouillet@users.noreply.github.com>
2024-10-16 23:21:37 +0200

    feat(hyprland): added mouse config

--- config/hyprland.nix
+++ config/hyprland.nix
@@ -22,6 +22,19 @@ in
         modifier = "SUPER";
       in
       ''
+        input {
+          kb_layout = ${keyboardLayout}
+          kb_options = grp:alt_shift_toggle
+          kb_options = caps:super
+          follow_mouse = 1
+          touchpad {
+            natural_scroll = true
+            disable_while_typing = true
+            scroll_factor = 0.8
+          }
+          sensitivity = 0 # -1.0 - 1.0, 0 means no modification.
+          accel_profile = flat
+        }
         monitor=,preferred,auto,1
         bind = ${modifier},Return,exec,${terminal}
         bind = ${modifier},W,exec,${browser}
