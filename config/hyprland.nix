81336c6544c608b049640d16ee9abd542a51c3f2
Ben Bouillet <15980664+benbouillet@users.noreply.github.com>
2024-11-07 20:19:36 +0100

    feat(hyprland): added cosmetic changes

--- config/hyprland.nix
+++ config/hyprland.nix
@@ -56,6 +56,33 @@ with lib;
             col.active_border = rgb(${config.lib.stylix.colors.base08}) rg(${config.lib.stylix.colors.base0C}) 45deg
             col.inactive_border = rgb(${config.lib.stylix.colors.base01})
           }
+          decoration {
+            rounding = 10
+            drop_shadow = true
+            shadow_range = 4
+            col.shadow = rgba(1a1a1aee)
+            blur {
+              enabled = true
+              size = 5
+              passes = 3
+              new_optimizations = on
+              ignore_opacity = off
+            }
+          }
+          animations {
+            enabled = yes
+            bezier = wind, 0.05, 0.9, 0.1, 1.05
+            bezier = winIn, 0.1, 1.1, 0.1, 1.1
+            bezier = winOut, 0.3, -0.3, 0, 1
+            bezier = liner, 1, 1, 1, 1
+            animation = windows, 1, 6, wind, slide
+            animation = windowsIn, 1, 6, winIn, slide
+            animation = windowsOut, 1, 5, winOut, slide
+            animation = windowsMove, 1, 5, wind, slide
+            animation = border, 1, 1, liner
+            animation = fade, 1, 10, default
+            animation = workspaces, 1, 5, wind
+          }
           gestures {
             workspace_swipe = true
             workspace_swipe_fingers = 3
