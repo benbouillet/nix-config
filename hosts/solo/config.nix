e2ff3af9b768993d144cba1ba9965e823cd2896e
Ben Bouillet <15980664+benbouillet@users.noreply.github.com>
2024-11-13 22:03:39 +0100

    feat(ui): modified fonts

--- hosts/solo/config.nix
+++ hosts/solo/config.nix
@@ -95,6 +95,27 @@ in
     cursor.package = pkgs.bibata-cursors;
     cursor.name = "Bibata-Modern-Ice";
     cursor.size = 24;
+    fonts = {
+      monospace = {
+        package = pkgs.fira-code;
+        name = "Fira Code";
+      };
+      sansSerif = {
+        package = pkgs.montserrat;
+        name = "Montserrat";
+      };
+      serif = {
+        package = pkgs.montserrat;
+        name = "Montserrat";
+      };
+      sizes = {
+        applications = 12;
+        terminal = 13;
+        desktop = 11;
+        popups = 12;
+      };
+    };
+
   };
 
   services = {
