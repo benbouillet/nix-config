d0de0d95e321bc32903bf0a194134405ebc65e18
Ben Bouillet <15980664+benbouillet@users.noreply.github.com>
2025-01-21 22:28:36 +0100

    fix(config): modularized stylix configuration

---
+++ hosts/modules/stylix.nix
@@ -0,0 +1,34 @@
+{ ... }: {
+  stylix = {
+    enable = true;
+    image = ../../files/wallpapers/${wallpaper_file};
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/${theme}.yaml";
+    cursor.package = pkgs.bibata-cursors;
+    cursor.name = "Bibata-Modern-Ice";
+    cursor.size = 24;
+    fonts = {
+      monospace = {
+        package = pkgs.nerdfonts;
+        name = "Fira Code";
+      };
+      sansSerif = {
+        package = pkgs.roboto;
+        name = "Roboto";
+      };
+      serif = {
+        package = pkgs.roboto-serif;
+        name = "Robot Serif";
+      };
+      emoji = {
+        name = "Noto Emoji";
+        package = pkgs.noto-fonts-monochrome-emoji;
+      };
+      sizes = {
+        applications = 12;
+        terminal = 13;
+        desktop = 11;
+        popups = 12;
+      };
+    };
+  };
+}
--- hosts/solo/configuration.nix
+++ hosts/solo/configuration.nix
@@ -21,58 +21,14 @@ in {
     ../modules/common.nix
     ../modules/desktop.nix
     ../modules/ssd.nix
+    ../modules/stylix.nix
     ../modules/tailscale.nix
 
     ./users.nix
   ];
 
-
   # Enable networking
   networking.hostName = host;
-
-  environment.pathsToLink = [
-    "/share/zsh"
-    "/share/xdg-desktop-portal"
-    "/share/applications"
-  ];
-
-  stylix = {
-    enable = true;
-    image = ../../files/wallpapers/${wallpaper_file};
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/${theme}.yaml";
-    cursor.package = pkgs.bibata-cursors;
-    cursor.name = "Bibata-Modern-Ice";
-    cursor.size = 24;
-    fonts = {
-      monospace = {
-        package = pkgs.nerdfonts;
-        name = "Fira Code";
-      };
-      sansSerif = {
-        package = pkgs.roboto;
-        name = "Roboto";
-      };
-      serif = {
-        package = pkgs.roboto-serif;
-        name = "Robot Serif";
-      };
-      emoji = {
-        name = "Noto Emoji";
-        package = pkgs.noto-fonts-monochrome-emoji;
-      };
-      sizes = {
-        applications = 12;
-        terminal = 13;
-        desktop = 11;
-        popups = 12;
-      };
-    };
-
-  };
-
-
-  
-  console.keyMap = "us";
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
@@ -81,5 +37,11 @@ in {
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.05"; # Did you read the comment?
+
+
+
+  
+  console.keyMap = "us";
+
 
 }
