169bda46e34073939524bad5e1cce50e979188a7
Ben Bouillet <15980664+benbouillet@users.noreply.github.com>
2025-03-06 19:14:39 +0100

    fix(global): adapted locales

--- modules/common.nix
+++ modules/common.nix
@@ -45,14 +45,14 @@
 
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "fr_FR.UTF-8";
-    LC_IDENTIFICATION = "fr_FR.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
     LC_MEASUREMENT = "fr_FR.UTF-8";
     LC_MONETARY = "fr_FR.UTF-8";
-    LC_NAME = "fr_FR.UTF-8";
+    LC_NAME = "en_US.UTF-8";
     LC_NUMERIC = "fr_FR.UTF-8";
-    LC_PAPER = "fr_FR.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
     LC_TELEPHONE = "fr_FR.UTF-8";
-    LC_TIME = "fr_FR.UTF-8";
+    LC_TIME = "en_US.UTF-8";
   };
 
   fonts = {
