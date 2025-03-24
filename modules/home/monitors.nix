2a883a9199ea473509347324ea68a37a8bd9c006
Ben Bouillet <15980664+benbouillet@users.noreply.github.com>
2025-03-24 17:35:27 +0100

    feat(hyprland): added default workspaces per monitor

--- modules/home/monitors.nix
+++ modules/home/monitors.nix
@@ -14,11 +14,11 @@
         "2,monitor:eDP-1"
         "3,monitor:eDP-1"
         "4,monitor:eDP-1"
-        "5,monitor:desc:LG Electronics LG HDR WQHD 312NTWG9Z889"
+        "5,monitor:desc:LG Electronics LG HDR WQHD 312NTWG9Z889,default:true"
         "6,monitor:desc:LG Electronics LG HDR WQHD 312NTWG9Z889"
         "7,monitor:desc:LG Electronics LG HDR WQHD 312NTWG9Z889"
         "8,monitor:desc:LG Electronics LG HDR WQHD 312NTWG9Z889"
-        "9,monitor:desc:Invalid Vendor Codename - RTK 0x1920 demoset-1"
+        "9,monitor:desc:Invalid Vendor Codename - RTK 0x1920 demoset-1,default:true"
         "10,desc:Invalid Vendor Codename - RTK 0x1920 demoset-1"
       ];
     };
