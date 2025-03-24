8f7a6a1c04459a7f3c8904064a812138981b92ed
Ben Bouillet <15980664+benbouillet@users.noreply.github.com>
2025-03-24 17:29:16 +0100

    feat(hyprland): binded workspaces to monitors

--- modules/home/monitors.nix
+++ modules/home/monitors.nix
@@ -9,6 +9,18 @@
         "desc:Invalid Vendor Codename - RTK 0x1920 demoset-1,1920x1080,1920x0,1"
         "desc:LG Electronics LG HDR WQHD 312NTWG9Z889,3440x1440,1920x0,1"
       ];
+      workspace = [
+        "1,monitor:eDP-1"
+        "2,monitor:eDP-1"
+        "3,monitor:eDP-1"
+        "4,monitor:eDP-1"
+        "5,monitor:desc:LG Electronics LG HDR WQHD 312NTWG9Z889"
+        "6,monitor:desc:LG Electronics LG HDR WQHD 312NTWG9Z889"
+        "7,monitor:desc:LG Electronics LG HDR WQHD 312NTWG9Z889"
+        "8,monitor:desc:LG Electronics LG HDR WQHD 312NTWG9Z889"
+        "9,monitor:desc:Invalid Vendor Codename - RTK 0x1920 demoset-1"
+        "10,desc:Invalid Vendor Codename - RTK 0x1920 demoset-1"
+      ];
     };
   };
 }
