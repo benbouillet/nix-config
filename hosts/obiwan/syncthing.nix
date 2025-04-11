1be35c5e2374b5cce4db9cd06411922f8e03ebc7
Ben Bouillet <15980664+benbouillet@users.noreply.github.com>
2025-04-11 11:46:13 +0200

    fix(syncthing): removed folder configuration

--- hosts/obiwan/syncthing.nix
+++ hosts/obiwan/syncthing.nix
@@ -18,20 +18,6 @@
         "pixel" = { id = "DAPOL32-UCPTKEC-UKN2ZI7-LFXCIT7-BACEC6R-R3KP7DC-JN7UEXS-ZXS6SA6"; };
         "macbookpro" = { id = "7WUXUQS-BPY7X37-5OMTXRQ-XOOBJE2-BQ4AYNV-RRCJPUI-K6X6X56-4XPOWQ4"; };
       };
-      folders = {
-        "toolbox" = { 
-          path = "/home/${username}/toolbox"; 
-          devices = [
-            "pixel"
-          ]; 
-          versioning = { 
-            type = "simple"; 
-            params = { 
-              keep = "10";
-            }; 
-          };
-        };
-      };
       options = {
         urAccepted = -1;
       };
