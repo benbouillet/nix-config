fb63ed63dd5136185d5319583e71643ec821d677
Ben Bouillet <15980664+benbouillet@users.noreply.github.com>
2025-04-12 15:59:40 +0200

    fix(scripts): lint

--- scripts/list-hyprland-bindings.nix
+++ scripts/list-hyprland-bindings.nix
@@ -1,17 +1,16 @@
-{ pkgs }:
-
+{pkgs}:
 pkgs.writeShellScriptBin "list-hyprland-bindings" ''
-    gen_bindings_list() {
-      cat ~/.config/hypr/hyprland.conf  |
-      awk -F, '
-        /bind/ {
-          gsub(/bind[dem]+=/,"");
-          print $3": "$1"+"$2
-        }' |
-      sort |
-    uniq |
-    ${pkgs.tofi}/bin/tofi --width "70%" --fuzzy-match false --prompt-text "Shortcut:"
-    }
+  gen_bindings_list() {
+    cat ~/.config/hypr/hyprland.conf  |
+    awk -F, '
+      /bind/ {
+        gsub(/bind[dem]+=/,"");
+        print $3": "$1"+"$2
+      }' |
+    sort |
+  uniq |
+  ${pkgs.tofi}/bin/tofi --width "70%" --fuzzy-match false --prompt-text "Shortcut:"
+  }
 
-    gen_bindings_list
+  gen_bindings_list
 ''
