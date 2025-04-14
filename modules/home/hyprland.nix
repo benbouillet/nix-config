5fa6c18401e8b8517bad3df97a176f3fb6ea0dd3
Ben Bouillet <15980664+benbouillet@users.noreply.github.com>
2025-04-14 11:09:14 +0200

    fix(monitors): re-added workspaces definition

--- modules/home/hyprland.nix
+++ modules/home/hyprland.nix
@@ -145,6 +145,18 @@ with lib;
         pseudotile = true;
         preserve_split = true;
       };
+      workspace = [
+        "1,monitor:eDP-1,default:true"
+        "2,monitor:eDP-1"
+        "3,monitor:eDP-1"
+        "4,monitor:eDP-1"
+        "5,monitor:desc:LG Electronics LG HDR WQHD 312NTWG9Z889,default:true"
+        "6,monitor:desc:LG Electronics LG HDR WQHD 312NTWG9Z889"
+        "7,monitor:desc:LG Electronics LG HDR WQHD 312NTWG9Z889"
+        "8,monitor:desc:LG Electronics LG HDR WQHD 312NTWG9Z889"
+        "9,monitor:desc:Invalid Vendor Codename - RTK 0x1920 demoset-1,default:true"
+        "10,monitor:desc:Invalid Vendor Codename - RTK 0x1920 demoset-1"
+      ];
     };
   };
 }
