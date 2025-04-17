14fdbf905ba0ef6eddd40188cd05ce2cdcead8b1
Ben Bouillet <15980664+benbouillet@users.noreply.github.com>
2025-04-17 14:40:27 +0200

    fix(network): improved wifi configuration

--- modules/nixos/wireless.nix
+++ modules/nixos/wireless.nix
@@ -12,12 +12,26 @@
       ensureProfiles = {
         environmentFiles = [ config.sops.secrets."wireless/home".path ];
         profiles = {
-          home-wifi = {
-            connection.id = "home-wifi";
-            connection.type = "wifi";
-            wifi.ssid = "$HOME_UUID";
+          "home-wifi" = {
+            connection = {
+              id = "home-wifi";
+              permissions = "user:ben:;";
+              type = "wifi";
+              uuid = "$HOME_UUID";
+            };
+            ipv4 = {
+              method = "auto";
+            };
+            ipv6 = {
+              addr-gen-mode = "default";
+              method = "auto";
+            };
+            proxy = { };
+            wifi = {
+              mode = "infrastructure";
+              ssid = "$HOME_UUID";
+            };
             wifi-security = {
-              auth-alg = "open";
               key-mgmt = "wpa-psk";
               psk = "$HOME_PSK";
             };
