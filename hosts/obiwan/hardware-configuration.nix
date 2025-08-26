11c64366814e360e6cc29b7488f6bfcbb6466e80
Ben Bouillet <15980664+benbouillet@users.noreply.github.com>
2025-08-26 14:43:57 +0200

    feat(obiwan): enabled hardware graphics

--- hosts/obiwan/hardware-configuration.nix
+++ hosts/obiwan/hardware-configuration.nix
@@ -12,32 +12,39 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "thunderbolt" "usb_storage" "sd_mod" ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-amd" ];
-  boot.extraModulePackages = [ ];
+  hardware = {
+    graphics = {
+      enable = true;
+      enable32Bit = true;
+    };
+  };
+  services.xserver.videoDrivers = ["amdgpu"];
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/d9b0adb4-95c0-46dc-9136-63663457afd3";
+  boot = {
+    initrd = {
+      availableKernelModules = [ "nvme" "xhci_pci" "thunderbolt" "usb_storage" "sd_mod" ];
+      kernelModules = [ ];
+    };
+    kernelModules = [ "kvm-amd" ];
+    extraModulePackages = [ ];
+  };
+
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-uuid/d9b0adb4-95c0-46dc-9136-63663457afd3";
       fsType = "ext4";
     };
 
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/9100-1694";
+    "/boot" = {
+      device = "/dev/disk/by-uuid/9100-1694";
       fsType = "vfat";
       options = [ "fmask=0077" "dmask=0077" ];
     };
+  };
 
   swapDevices =
     [ { device = "/dev/disk/by-uuid/e6f07af8-112b-4658-80a2-806464e00368"; }
     ];
-
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp1s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
