8769c69ffa69f54cfd8889a306aaa2aed3f6a0fc
Ben Bouillet <15980664+benbouillet@users.noreply.github.com>
2025-08-26 14:36:52 +0200

    fix(nixvim): option path change

--- modules/home/neovim/completion.nix
+++ modules/home/neovim/completion.nix
@@ -12,15 +12,17 @@
       lspkind = {
         enable = true;
 
-        cmp = {
-          enable = true;
-          menu = {
-            nvim_lsp = "[LSP]";
-            nvim_lua = "[api]";
-            path = "[path]";
-            luasnip = "[snip]";
-            buffer = "[buffer]";
-            neorg = "[neorg]";
+        settings = {
+          cmp = {
+            enable = true;
+            menu = {
+              nvim_lsp = "[LSP]";
+              nvim_lua = "[api]";
+              path = "[path]";
+              luasnip = "[snip]";
+              buffer = "[buffer]";
+              neorg = "[neorg]";
+            };
           };
         };
       };
