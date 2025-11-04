0abf7e5546fdec2c2bca8c0abdc4187c2c40307a
Ben Bouillet <15980664+benbouillet@users.noreply.github.com>
2025-11-04 14:15:42 +0100

    feat(networking): simplified desktop configs

--- hosts/obiwan/networking.nix
+++ hosts/obiwan/networking.nix
@@ -7,15 +7,7 @@
   sops.secrets."wireless/home" = {};
 
   networking = {
-    # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-    # (the default) this is the recommended approach. When using systemd-networkd it's
-    # still possible to use this option, but it's recommended to use it in conjunction
-    # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-    useDHCP = lib.mkDefault true;
-
     networkmanager = {
-      enable = true;
-
       wifi.powersave = true;
 
       ensureProfiles = {
