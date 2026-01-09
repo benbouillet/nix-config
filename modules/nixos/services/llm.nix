0282d79ce4779c7dbfb3be490cf344dce6c16eeb
Ben Bouillet <15980664+benbouillet@users.noreply.github.com>
2026-01-09 17:00:32 +0100

    feat(chewie): switched llms to containers

--- modules/nixos/services/llm.nix
+++ modules/nixos/services/llm.nix
@@ -4,45 +4,121 @@
 }:
 let
   domain = "r4clette.com";
-  port = 11434;
+  ollama_port = 9020;
+  openwebui_port = 9021;
+  rootVolumesPath = "/srv/containers";
 in
 {
   systemd.tmpfiles.rules = lib.mkAfter [
-    "d /srv/llm/ 2775 ollama ollama - -"
+    "d /srv/llm/ 2775 root zfsmnt - -"
+    "d ${rootVolumesPath}/openwebui/ 2775 root zfsmnt - -"
+    "d ${rootVolumesPath}/ollama/ 2775 root zfsmnt - -"
   ];
 
-  services.ollama = {
-    enable = true;
-    port = port;
-    loadModels = [
-      # "llama2:13b"
-      "llama3.1:8b"
-      # "llama3.2:3"
-      # "codellama:13b"
-      # "codellama:7b"
-      # "zongwei/gemma3-translator:4b"
-      # "qwen3:14b"
-      # "qwen3:8b"
-    ];
-    syncModels = true;
-    models = "/srv/llm";
-    environmentVariables = {
-      OLLAMA_KEEP_ALIVE = "10m";
-      OLLAMA_FLASH_ATTENTION = "1";
-      OLLAMA_KV_CACHE_TYPE = "q8_0";
-      OLLAMA_HOST = "127.0.0.1:${toString port}";
+  virtualisation.oci-containers.containers = {
+    "ollama" = {
+      image = "ollama/ollama:0.13.5";
+      ports = [
+        "127.0.0.1:${toString ollama_port}:11434"
+      ];
+      devices = [
+        "nvidia.com/gpu=all"
+      ];
+      volumes = [
+        "${rootVolumesPath}/ollama:/root/.ollama:rw"
+        "/srv/llm/:/usr/share/ollama/.ollama/models:rw"
+      ];
+      environment = {
+        OLLAMA_MODELS = "/usr/share/ollama/.ollama/models";
+        OLLAMA_KEEP_ALIVE = "10m";
+        OLLAMA_FLASH_ATTENTION = "1";
+        OLLAMA_KV_CACHE_TYPE = "q8_0";
+      };
+    };
+    "open-webui" = {
+      image = "ghcr.io/open-webui/open-webui:v0.6.43-slim";
+      ports = [
+        "127.0.0.1:${toString openwebui_port}:8080"
+      ];
+      volumes = [
+        "${rootVolumesPath}/openwebui/:/app/backend/data:rw"
+      ];
+      environment = {
+        OLLAMA_MODELS = "/usr/share/ollama/.ollama/models";
+        OLLAMA_KEEP_ALIVE = "10m";
+        OLLAMA_FLASH_ATTENTION = "1";
+        OLLAMA_KV_CACHE_TYPE = "q8_0";
+      };
     };
   };
+  # services = {
+  #   ollama = {
+  #     enable = true;
+  #     port = ollama_port;
+  #     loadModels = [
+  #       # "llama2:13b"
+  #       "llama3.1:8b"
+  #       # "llama3.2:3"
+  #       # "codellama:13b"
+  #       # "codellama:7b"
+  #       # "zongwei/gemma3-translator:4b"
+  #       # "qwen3:14b"
+  #       # "qwen3:8b"
+  #     ];
+  #     syncModels = true;
+  #     models = "/srv/llm";
+  #     environmentVariables = {
+  #       OLLAMA_KEEP_ALIVE = "10m";
+  #       OLLAMA_FLASH_ATTENTION = "1";
+  #       OLLAMA_KV_CACHE_TYPE = "q8_0";
+  #       OLLAMA_HOST = "127.0.0.1:${toString ollama_port}";
+  #     };
+  #   };
+  #   open-webui = {
+  #     enable = true;
+  #     port = openwebui_port;
+  #     host = "127.0.0.1:${toString openwebui_port}";
+  #     environment = {
+  #       OLLAMA_API_BASE_URL = "http://127.0.0.1:${toString ollama_port}";
+  #     };
+  #     stateDir = "${rootVolumesPath}/openwebui/";
+  #   };
+  # };
 
-  services.caddy.virtualHosts."ollama.${domain}".extraConfig = ''
-    reverse_proxy 127.0.0.1:${toString port} {
-      # Make Ollama see the request as local
-      header_up Host 127.0.0.1:${toString port}
+  services.caddy.virtualHosts."*.${domain}".extraConfig = lib.mkAfter ''
+    @ollama host ollama.${domain}
+    handle @ollama {
+      reverse_proxy 127.0.0.1:${toString ollama_port}
+    }
 
-      # Don’t leak client identity to upstream (can trigger “remote” policy)
-      header_up -X-Forwarded-For
-      header_up -X-Real-IP
-      header_up -Forwarded
+    @open-webui host chat.${domain}
+    handle @open-webui {
+      reverse_proxy 127.0.0.1:${toString openwebui_port}
     }
   '';
+
+  # services.caddy.virtualHosts = {
+  #   "ollama.${domain}".extraConfig = ''
+  #     reverse_proxy 127.0.0.1:${toString ollama_port} {
+  #       # Make Ollama see the request as local
+  #       header_up Host 127.0.0.1:${toString ollama_port}
+  #
+  #       # Don’t leak client identity to upstream (can trigger “remote” policy)
+  #       header_up -X-Forwarded-For
+  #       header_up -X-Real-IP
+  #       header_up -Forwarded
+  #     }
+  #   '';
+  #   "chat.${domain}".extraConfig = ''
+  #     reverse_proxy 127.0.0.1:${toString openwebui_port} {
+  #       # Make Ollama see the request as local
+  #       header_up Host 127.0.0.1:${toString openwebui_port}
+  #
+  #       # Don’t leak client identity to upstream (can trigger “remote” policy)
+  #       header_up -X-Forwarded-For
+  #       header_up -X-Real-IP
+  #       header_up -Forwarded
+  #     }
+  #   '';
+  # };
 }
