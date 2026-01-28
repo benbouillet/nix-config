532f3cb887ad20ce2069c1c8e2020799fb5a7be8
Ben Bouillet <15980664+benbouillet@users.noreply.github.com>
2026-01-28 12:22:50 +0100

    feat(chewie): made tailscale config more robust

--- modules/nixos/tailscale.nix
+++ modules/nixos/tailscale.nix
@@ -1,18 +1,16 @@
 {
   username,
-  config,
   host,
   ...
 }:
 {
-  sops.secrets."tailscale/${host}" = {};
+  sops.secrets."tailscale/${host}" = { };
 
   services = {
     tailscale = {
       enable = true;
       extraSetFlags = [ "--operator=${username}" ];
       extraUpFlags = [ "--operator=${username}" ];
-      authKeyFile = config.sops.secrets."tailscale/${host}".path ;
     };
   };
 }
