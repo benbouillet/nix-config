2bf8be30287f763d5a58c98d2f8a57a8e750efb8
Ben Bouillet <15980664+benbouillet@users.noreply.github.com>
2026-02-04 19:51:43 +0100

    fix(chewie): made redis available to podman containers

--- modules/nixos/services/redis.nix
+++ modules/nixos/services/redis.nix
@@ -4,11 +4,17 @@
   ...
 }:
 {
+  networking.firewall.interfaces."podman0".allowedTCPPorts = [ globals.ports.redis ];
+
   services.redis = {
     package = pkgs.valkey;
     servers."raclette" = {
       enable = true;
       port = globals.ports.redis;
+      bind = "0.0.0.0";
+      settings = {
+        "protected-mode" = "no";
+      };
     };
   };
 }
