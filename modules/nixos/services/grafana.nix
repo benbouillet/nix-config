7a31a24fbbd7ff2c8a1cd6f7bfa1bc7b00e512c7
Ben Bouillet <15980664+benbouillet@users.noreply.github.com>
2026-02-27 21:19:17 +0100

    feat(grafana): implemented OIDC

--- modules/nixos/services/grafana.nix
+++ modules/nixos/services/grafana.nix
@@ -5,10 +5,17 @@
   ...
 }:
 {
-  sops.secrets."grafana/secret_key" = {
-    mode = "0400";
-    owner = "grafana";
-    group = "grafana";
+  sops.secrets = {
+    "grafana/secret_key" = {
+      mode = "0400";
+      owner = "grafana";
+      group = "grafana";
+    };
+    "grafana/oidc_secret" = {
+      mode = "0400";
+      owner = "grafana";
+      group = "grafana";
+    };
   };
 
   services.grafana = {
@@ -20,6 +27,8 @@
         enforce_domain = true;
         enable_gzip = true;
         domain = "grafana.${globals.domain}";
+        root_url = "https://grafana.${globals.domain}/";
+        serve_from_sub_path = false;
       };
       auth = {
         disable_login_form = true;
@@ -27,6 +36,52 @@
       };
       security = {
         secret_key = "\$__file{${config.sops.secrets."grafana/secret_key".path}}";
+      };
+
+      "auth.generic_oauth" = {
+        enabled = true;
+        name = "Authelia";
+        icon = "signin";
+
+        client_id = "grafana";
+        client_secret = "\$__file{${config.sops.secrets."grafana/oidc_secret".path}}";
+
+        scopes = [
+          "openid"
+          "profile"
+          "email"
+          "groups"
+        ];
+
+        empty_scopes = false;
+        auth_url = "https://auth.${globals.domain}/api/oidc/authorization";
+        token_url = "https://auth.${globals.domain}/api/oidc/token";
+        api_url = "https://auth.${globals.domain}/api/oidc/userinfo";
+        login_attribute_path = "preferred_username";
+        groups_attribute_path = "groups";
+        name_attribute_path = "name";
+        use_pkce = true;
+        role_attribute_path = "contains(groups[*], 'monitoring') && 'GrafanaAdmin'";
+        auth_style = "InHeader";
+      };
+    };
+    provision = {
+      datasources.settings = {
+        deleteDatasources = [
+          {
+            name = "Prometheus";
+            orgId = 1;
+          }
+        ];
+        datasources = [
+          {
+            url = "http://localhost:${toString globals.ports.prometheus}";
+            type = "prometheus";
+            name = "Prometheus";
+            editable = false;
+            access = "proxy";
+          }
+        ];
       };
     };
   };
@@ -41,6 +96,38 @@
         }
       ];
     };
+
+    identity_providers.oidc.cors.allowed_origins = [
+      "https://grafana.${globals.domain}"
+    ];
+
+    identity_providers.oidc.clients = [
+      {
+        client_id = "grafana";
+        client_name = "Grafana";
+        client_secret = "$pbkdf2-sha512$310000$cGFjKBm6mN9N0HMB2MzVwA$WDcbkc8LQI6lt8pZAMt9EYvnlH7uA0LpydmW0.YqlZR3LW9j8XcWuuXzCTv84pxDTNzqGFf5CvSn2muabC.1iA";
+        public = false;
+        authorization_policy = "one_factor";
+        require_pkce = true;
+        pkce_challenge_method = "S256";
+        redirect_uris = [ "https://grafana.${globals.domain}/login/generic_oauth" ];
+        scopes = [
+          "openid"
+          "profile"
+          "groups"
+          "email"
+        ];
+        response_types = [
+          "code"
+        ];
+        grant_types = [
+          "authorization_code"
+        ];
+        access_token_signed_response_alg = "none";
+        userinfo_signed_response_alg = "none";
+        token_endpoint_auth_method = "client_secret_basic";
+      }
+    ];
   };
 
   services.caddy.virtualHosts."*.${globals.domain}".extraConfig = lib.mkAfter ''
