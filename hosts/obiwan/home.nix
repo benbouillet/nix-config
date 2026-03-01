6cf7842e7fd6b0266cd7f9b8bc5c5d55ffca5939
Ben Bouillet <15980664+benbouillet@users.noreply.github.com>
2026-03-01 11:45:24 +0100

    feat(obiwan): improved ssh config for remote hosts

--- hosts/obiwan/home.nix
+++ hosts/obiwan/home.nix
@@ -57,6 +57,9 @@
     };
     extraConfig = ''
       Include ~/.ssh/chewie.conf
+      Include ~/.ssh/chewie-local.conf
+      Include ~/.ssh/yoda.conf
+      Include ~/.ssh/yoda-local.conf
       Include ~/.ssh/jellybox.conf
     '';
   };
--- hosts/obiwan/ssh.nix
+++ hosts/obiwan/ssh.nix
@@ -10,6 +10,24 @@
       mode = "0400";
       path = "/home/ben/.ssh/chewie.conf";
     };
+    "ssh/yoda" = {
+      owner = username;
+      group = "users";
+      mode = "0400";
+      path = "/home/ben/.ssh/yoda.conf";
+    };
+    "ssh/chewie-local" = {
+      owner = username;
+      group = "users";
+      mode = "0400";
+      path = "/home/ben/.ssh/chewie-local.conf";
+    };
+    "ssh/yoda-local" = {
+      owner = username;
+      group = "users";
+      mode = "0400";
+      path = "/home/ben/.ssh/yoda-local.conf";
+    };
     "ssh/jellybox" = {
       owner = username;
       group = "users";
