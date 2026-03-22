635d1a242a9c0133c48d0e4a7582480aed6ed75c
Ben Bouillet <15980664+benbouillet@users.noreply.github.com>
2026-03-22 22:18:51 +0100

    fix(redis): improved reliability with dependency on podman network

--- modules/nixos/services/redis.nix
+++ modules/nixos/services/redis.nix
@@ -1,14 +1,28 @@
 {
   pkgs,
+  config,
   globals,
   ...
 }:
 {
   networking.firewall.interfaces."podman0".allowedTCPPorts = [ globals.ports.redis ];
 
-  systemd.services.redis-raclette = {
+  systemd.services.podman-bridge-ready = {
+    description = "Ensure podman bridge network exists";
     after = [ "podman.service" ];
     requires = [ "podman.service" ];
+    before = [ "redis-raclette.service" ];
+    wantedBy = [ "redis-raclette.service" ];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+      ExecStart = "${config.virtualisation.podman.package}/bin/podman network create --ignore podman";
+    };
+  };
+
+  systemd.services.redis-raclette = {
+    after = [ "podman-bridge-ready.service" ];
+    requires = [ "podman-bridge-ready.service" ];
   };
 
   services.redis = {
