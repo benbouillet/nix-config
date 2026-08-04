97953b28fac5c01cc72f2a9daf4f6dcd0c35dbb7
Ben Bouillet <15980664+benbouillet@users.noreply.github.com>
2026-08-04 14:03:15 +0200

    chore(desktop): removed unused keepassxc package

--- modules/home/desktop.nix
+++ modules/home/desktop.nix
@@ -34,35 +34,6 @@
         font-size = 14;
       };
     };
-    keepassxc = {
-      enable = true;
-      settings = {
-        Browser.Enabled = true;
-        GUI = {
-          AdvancedSettings = true;
-          ApplicationTheme = "dark";
-          CompactMode = true;
-          HidePasswords = true;
-          AutoSaveOnExit = true;
-        };
-        Security = {
-          LockDatabaseIdle = true;
-          LockDatabaseIdleSeconds = 600;
-          MinimizeOnCopy = true;
-        };
-        PasswordGenerator = {
-          AdvancedMode = true;
-          LowerCase = true;
-          UpperCase = true;
-          Numbers = true;
-          Logograms = false;
-          EASCII = false;
-          Dashes = true;
-          Length = 32;
-        };
-        SSHAgent.Enabled = false;
-      };
-    };
     starship = {
       enable = true;
       enableZshIntegration = true;
