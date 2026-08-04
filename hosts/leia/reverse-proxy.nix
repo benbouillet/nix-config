0ba92c459ecbc6cc3031cdf4b05386ff768e8a1f
Ben Bouillet <15980664+benbouillet@users.noreply.github.com>
2026-08-04 10:19:15 +0200

    fix(caddy): bumped cloudflare plugin version

--- hosts/leia/reverse-proxy.nix
+++ hosts/leia/reverse-proxy.nix
@@ -8,7 +8,7 @@
 let
   caddyWithCloudflare = pkgs.caddy.withPlugins {
     plugins = [ "github.com/caddy-dns/cloudflare@v0.2.2" ];
-    hash = "sha256-7g8zDx5RhbptXFyEPtexxkHX8hw/gF001bZ7wX4Mjhs=";
+    hash = "sha256-mqIa0wI/VfjDblg0NnkzKllWHXZZPLwHP8xEVSwZuPE=";
   };
 in
 {
