b5c93843f51a203d9fa34573631e51f2fe7ed017
Ben Bouillet <15980664+benbouillet@users.noreply.github.com>
2026-08-23 11:56:12 +0200

    chore(hyprland): improved monitors management

--- hosts/obiwan/monitors.nix
+++ hosts/obiwan/monitors.nix
@@ -5,9 +5,24 @@
   wayland.windowManager.hyprland = {
     settings = {
       monitor = [
-        "eDP-1,2256x1504,0x0,1"
-        "desc:Invalid Vendor Codename - RTK 0x1920 demoset-1,1920x1080,2256x60,1"
-        "desc:LG Electronics LG HDR WQHD 312NTWG9Z889,3440x1440,2256x60,1"
+        {
+          output = "eDP-1";
+          mode = "2256x1504";
+          position = "0x0";
+          scale = 1;
+        }
+        {
+          output = "desc:Invalid Vendor Codename - RTK 0x1920 demoset-1";
+          mode = "1920x1080";
+          position = "2256x60";
+          scale = 1;
+        }
+        {
+          output = "desc:LG Electronics LG HDR WQHD 312NTWG9Z889";
+          mode = "3440x1440";
+          position = "2256x60";
+          scale = 1;
+        }
       ];
     };
   };
